2286a3350091e6e9d89cf9ac514fdc4d1ac6deb3
Ezequiel Amin <ezequielamin@outlook.com>
2023-12-01 16:38:50 -0300

    fix: add randomness to UNTIE votes

--- src/components/List/styled.ts
+++ src/components/List/styled.ts
@@ -2,18 +2,18 @@ import styled from '@emotion/styled';
 
 export const List = styled.ul`
   mb: 3;
-  max-height: calc(100vh - 200px);
+  max-height: calc(100dvh - 200px);
   overflow-y: scroll;
 
   @media (width < 768px) {
-    max-height: calc(100vh - 200px);
+    max-height: calc(100dvh - 200px);
   }
 
   @media (width <= 403px) {
-    max-height: calc(100vh - 250px);
+    max-height: calc(100dvh - 250px);
   }
 
   @media (width <= 350px) {
-    max-height: calc(100vh - 275px);
+    max-height: calc(100dvh - 275px);
   }
 `;
